7bf52dc0d745c31888ba7f94e8f7ec6df1c6381c
vaarigupta <vaarigupta24@gmail.com>
2018-09-27 18:26:55 +0530

    app changed

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const request = require('request');
-
 request({
-	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=%201301%20lombard%20street%20philadelphia',
-	json : true 
-}, (error, response , body)=>{
-	console.log(JSON.stringify(body , undefined , 2))
-
+	url : "https://maps.googleapis.com/maps/api/geocode/json?address=%20shahdara",
+	json : true
+},(error,response , body)=>{
+	//console.log('error:', error); // Print the error if one occurred
+  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  console.log(JSON.stringify(body,undefined,4)); // Print the HTML for the Google homepage.
+   
 })
+
+
+
+
+
+
+
+
+
+
+
+
 
 
