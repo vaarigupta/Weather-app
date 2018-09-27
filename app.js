dff0c9ce49f6a6d48d067606e3cf1930399d0380
vaarigupta <vaarigupta24@gmail.com>
2018-09-28 01:17:49 +0530

    error handling

--- app.js
+++ app.js
@@ -26,9 +26,26 @@ request({
 	//console.log('error:', error); // Print the error if one occurred
   //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
   //console.log(body)
-  console.log(`Address : ${body.results[0].formatted_address}`); // Print the HTML for the Google homepage.
-  console.log(`Longitude : ${body.results[0].geometry.location.lat}`)
-  console.log(`Latitude : ${body.results[0].geometry.location.lng}`)
+  if(error)
+  {
+  	console.log("Oops !! Unable to connect with google server ");
+  	console.log("please Check URL or your internet connection");
+  }
+  else if(body.status === 'ZERO_RESULTS')
+  {
+    console.log("Oops !! Unable to find location for this address")
+  }
+  else if(body.status==='OK')
+  {
+  	console.log(`Address : ${body.results[0].formatted_address}`); // Print the HTML for the Google homepage.
+  	console.log(`Longitude : ${body.results[0].geometry.location.lat}`);
+  	console.log(`Latitude : ${body.results[0].geometry.location.lng}`);
+  }
+  else
+  {
+  	console.log("Oops!!!!!!!!!!");
+  }
+ 
 })
 
 
