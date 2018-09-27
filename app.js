16af763b62f9de1ab5e3160c22d9d5fc92763db7
vaarigupta <vaarigupta24@gmail.com>
2018-09-28 04:38:35 +0530

    darksky api used for weather forecasting

--- app.js
+++ app.js
@@ -37,9 +37,28 @@ request({
 	json:true
 },(error, response,body)=>{
 	//console.log(JSON.stringify(body,undefined,3));
-	console.log(`Temperature - ${body.currently.temperature}`);
+	if(error)
+	{
+		///its is system related error where server is involved and its not 
+		///responding and we cannot reach to the server due to internet conncetion or wrong url
+		console.log("Oops !!! Unable to connect with server");
+	}
+	else if(response.statusCode === 400)
+	{
+
+		///user makes a mistake while fetching location  like 
+		// enter wrong location parameters
+       console.log("Oops !!! Unable to fetch weather ");
+	}
+	else if (!error && response.statusCode === 200) 
+	{
+    console.log(`Temperature - ${body.currently.temperature}`);
 	console.log(`Humidity - ${body.currently.humidity}`);
 	console.log(`Pressure - ${body.currently.pressure}`);
+
+
+	}
+	
 
 
 })
