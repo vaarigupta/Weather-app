dbf3b981d265c252a9a8daf4df87e571b65c7a71
vaarigupta <vaarigupta24@gmail.com>
2018-09-27 23:12:15 +0530

    yargs added

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ request({
 },(error,response , body)=>{
 	//console.log('error:', error); // Print the error if one occurred
   //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log(JSON.stringify(body,undefined,4)); // Print the HTML for the Google homepage.
-   
+  console.log(body)
+  console.log(`Address : ${body.results[0].formatted_address}`); // Print the HTML for the Google homepage.
+  console.log(`Longitude : ${body.results[0].geometry.location.lat}`)
+  console.log(`Latitude : ${body.results[0].geometry.location.lng}`)
 })
 
 
