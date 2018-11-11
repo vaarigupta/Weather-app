1c9d1328ba6ae4520251626636deb74e7f19f85e
vaarigupta <vaarigupta24@gmail.com>
2018-11-11 18:21:01 +0000

    modified

--- playground/promise.js
+++ playground/promise.js
@@ -1,22 +1,22 @@
-// var promiseObj = new Promise((resolve , reject)=>{
+var promiseObj = new Promise((resolve , reject)=>{
 	
-// 	setTimeout(()=>{
-// 		resolve("hey Im vaari");
-//         reject("unable to give Promises");
+	setTimeout(()=>{
+		resolve("hey Im vaari");
+        reject("unable to give Promises");
 
 
-// 	},2500);
+	},2500);
          
 	
-// });
+});
 
-// promiseObj.then((msg)=>{
-//    console.log("Success  : " ,msg)
-// }).then((errorMsg)=>{
-//   console.log( "Error Message : " ,errorMsg);
-// })
+promiseObj.then((msg)=>{
+   console.log("Success  : " ,msg)
+}).then((errorMsg)=>{
+  console.log( "Error Message : " ,errorMsg);
+})
 
-
+//-----------------------------------------------------------------
 var asyncAdd = (a,b)=>
 {
   return new Promise((resolve,reject)=>{
@@ -32,7 +32,7 @@ var asyncAdd = (a,b)=>
        }
   })
 }
-
+///------------------------------------------------------------------------------------------
 /// In the following code , when first promise call is rejected then other
 // call is succeeded irrespective of result come out from the previous promise
 // kayde se ,the next promise should not be executed but it executed because of the two call back functions
@@ -40,17 +40,18 @@ var asyncAdd = (a,b)=>
 /// we will resolve the following code by using catch() method in the 
 ///following code
 
-// asyncAdd(4,"8").then((res)=>{
-//   console.log("Result : " + res);
-//   return asyncAdd(res,'8');
-// },(errorMsg)=>{
-//   console.log(errorMsg);
-// }).then((res)=>{
-//  console.log("Result : " + res);
-// },(errorMsg)=>{
-//  console.log(errorMsg);
-// });
+asyncAdd(4,"8").then((res)=>{
+  console.log("Result : " + res);
+  return asyncAdd(res,'8');
+},(errorMsg)=>{
+  console.log(errorMsg);
+}).then((res)=>{
+ console.log("Result : " + res);
+},(errorMsg)=>{
+ console.log(errorMsg);
+});
 
+///------------------------------------------------------------------------------------------
 
 ///catch method is used for calling a callback function 
 ///for any of the rejection occurred in the promise 
